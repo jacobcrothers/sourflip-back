d9293ab29858c5952ba2b1b97425112435031ab8
jacobcrothers <jacobcrothers425@gmail.com>
2022-07-08 02:10:36 +0900

    update

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('dd3momogl4484p', 'postgres', 'cf0361e3d07a4d388e1d5d152f95881682c2aa18c378e5ce61653e31365003be', {
+const db = new Sequelize('dd3momogl4484p', 'vnmocjazdxqygp', 'cf0361e3d07a4d388e1d5d152f95881682c2aa18c378e5ce61653e31365003be', {
   host: 'ec2-54-159-22-90.compute-1.amazonaws.com',
-  dialect: 'postgres'
+  dialect: 'vnmocjazdxqygp'
 });
 
 module.exports = db;
